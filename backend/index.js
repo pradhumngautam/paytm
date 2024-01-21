828281f6dedeee48c832f85f921958fd8a897023
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-01-21 20:19:44 +0530

    routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const rootRouter = require('./routes/index');
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 app.use("/api/vi", rootRouter);
+
+app.listen(3000);
 
 
