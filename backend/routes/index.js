45d8c7229b1e3cfbce982d7e4e453de559f985b9
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-01-23 23:53:09 +0530

    commit

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const userRouter = require("./user");
 const accountRouter = require("./account");
 const router = express.Router();
