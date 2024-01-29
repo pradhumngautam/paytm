b4fab41dd0f89de570bc47a10631d64c6f7656d2
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-01-29 12:57:49 +0530

    signin fixed

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,23 @@
 import { Appbar } from "../components/Appbar";
 import { Balance } from "../components/Balance";
 import { Users } from "../components/Users";
+import { useEffect, useState, } from "react";
+import axios from "axios";
 
 export const Dashboard = () => {
+  const [balance, setBalance] = useState(100000);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/v1/account/balance")
+      .then((response) => {
+        setBalance(response.balance);
+      });
+  }, [balance]);
   return (
     <div>
       <Appbar />
       <div className="m-8">
-        <Balance value={"10,000"} />
+        <Balance value={balance} />
         <Users />
       </div>
     </div>
