0653119f893c9f761399aa116a5f079fba16d492
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-02-23 01:49:49 +0530

    health endpoint

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1", rootRouter);
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Succes!" });
+});
+
 app.listen(3000, () => {
   console.log(`http://localhost:${process.env.PORT}`);
 });
